e3bd6c8f595f679f04d07de8373f143bd1afd7b2
subrockmann <newinmunich@aol.com>
2023-02-06 19:33:15 +0100

    fix: fix hardcoded dropout

--- workbench/tensorflow.py
+++ workbench/tensorflow.py
@@ -8,6 +8,14 @@ def set_batchnorm_momentum(model, momentum=0.9):
             layer.momentum=momentum
     return model
 
+def set_dropout(model, dropout=0.2):
+    for layer in model.layers:
+        if type(layer)==type(tf.keras.layers.Dropout(0)):
+            #print(layer.momentum)
+            layer.rate = dropout
+    return model
+
+
 def get_layer_details_df(filepath):
     print(f"Reading in {filepath}")
     df = pd.read_csv(filepath)
